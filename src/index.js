ea9e0743323bee69fd82dc71cf6e4eac4e37051d
Olya Virchenko <olyavirchenko@192.168.1.12>
2024-02-28 00:50:30 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import configurStore from './redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={configurStore()}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
